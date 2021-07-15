cb5c6aee73f81d7a9921a9992f2d70baabde7883
anson-li <ansonli.94@gmail.com>
2021-07-14 19:31:40 -0600

    Update index.js

--- src/common/TeamSkills/index.js
+++ src/common/TeamSkills/index.js
@@ -30,31 +30,31 @@ class TeamSkills extends PureComponent {
         <div className={styles['team-skills-descriptor']}>
           I&apos;ll admit, I was a Google apologist and still love the company&apos;s game. But this is so much more than Google.
           <br /><br />
-          Google is like that 10 you see at a bar and decide to shoot your shot with. You go for him, becase why not? He's newly
+          Google is like that 10 you see at a bar and decide to shoot your shot with. You go for him, becase why not? He&apos;s newly
           available when you thought he was locked up forever. So you make a move, and surprisingly he says yes. You&apos;re shocked.
           Complete bliss. Everything is being taken to the next level. He gets your SEO firing on all cylinders.
-          The customer experience is amazing. He's doing things you never dreamed of. Soon, he starts telling people he could
+          The customer experience is amazing. He&apos;s doing things you never dreamed of. Soon, he starts telling people he could
           really see things go seriously with you. But then after a while you start to notice things.
           <br /><br />
           Google starts to act unusual. He says he&apos;s not mad but then starts getting passive aggressive towards you.
           Then if you get upset it&apos;s your fault. He&apos;s into some weird ideas about how he wants to live his life
           that you can&apos;t get on board with. His long term plans start to change, even though he&apos;ll never admit it and
-          still says he loves you, but he&apos;s never really there anymore. When you&apos;re at your lowest, he isn&apos;t 
-          even there to cheer on from the sideline. You get frustrated. You know the end is coming, but you can&apos;t admit it even when 
+          still says he loves you, but he&apos;s never really there anymore. When you&apos;re at your lowest, he isn&apos;t
+          even there to cheer on from the sideline. You get frustrated. You know the end is coming, but you can&apos;t admit it even when
           your friends tell you it&apos;s time. Then, one day, he&apos;s finally gone.
           <br /><br />
           So now you&apos;re back on the market. You&apos;re finally free of some stress, but at the same time you&apos;re alone and lost again.
-          You had your whole life planned out with Google, and now he&apos;s run off with the suddenly sexy YouTube. You know, the same YouTube 
+          You had your whole life planned out with Google, and now he&apos;s run off with the suddenly sexy YouTube. You know, the same YouTube
           who were ugly in high school. Yeah, things really do change, don&apos;t they? So, with nothing else to do, you go out for a drink.
           You&apos;re devastated. You thought you had it all figured out. And then he just up and left you. Not a clue what you want to do now.
           Then all of a sudden you hear a soft and soothing voice. &lsquo;You look like you could use a friend tonight.&rsquo; You turn around.
-          It's Ubeshi. He&apos;s giving you the smile that you give someone when you really care. You tell him all about what you've been going
-          through, how you got hurt. He tells you that you deserve better, and that someone will be there for you. He takes your hand. 
+          It&apos;s Ubeshi. He&apos;s giving you the smile that you give someone when you really care. You tell him all about what you&apos;ve been going
+          through, how you got hurt. He tells you that you deserve better, and that someone will be there for you. He takes your hand.
           You smile and take it back. You say thank you and that you really needed him tonight.
           <br /><br />
           Ubeshi takes you home and gets Jada a job. But you tell him you&apos;re still upset. You&apos;re not in the mood. He says that&apos;s ok.
           He leaves the room and you fall asleep. You wake up a little bit later, with a mild hangover. You look around, trying to remember where you are.
-          Then you see him. Ubeshi walks through the door. &lsquo;I thought you might want this,&rsquo; he says as he hands you a glass of water. He 
+          Then you see him. Ubeshi walks through the door. &lsquo;I thought you might want this,&rsquo; he says as he hands you a glass of water. He
           slides in next to you and wraps his arms around you. For the first time in a long time, you feel safe. You feel loved.
           <br /><br />
           That&apos;s the Ubeshi experience. And I cannot wait to see it play out.
